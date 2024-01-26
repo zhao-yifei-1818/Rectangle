95368dcdcb38c2ecdd97e886beca8a4c84177b37
zhao-yifei-1818 <williamfei0000@gmail.com>
2024-01-26 03:18:35 +0000

    Jan 25, 2024, 7:18 PM

--- tests.cpp
+++ tests.cpp
@@ -88,7 +88,6 @@ TEST_CASE("Rectangle getPerimeter()")
   REQUIRE(r2.getPerimeter() == Approx(8));
 }
 
-/*
 TEST_CASE("Rectangle translate()")
 {
   // Build rectangle
@@ -100,7 +99,6 @@ TEST_CASE("Rectangle translate()")
   REQUIRE(upperleft.getX() == Approx(3));
   REQUIRE(upperleft.getY() == Approx(10));
 }
-*/
 
 TEST_CASE("Rectangle contains() just touching")
 {
